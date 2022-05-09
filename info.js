70b9b57c0db253e63a210336000b4946ab2b1cc2
Mona Lisa <Mona@Lisa>
2022-05-09 11:40:18 +0800

    Bug Fixed

--- info.js
+++ info.js
@@ -3,7 +3,7 @@ var state;
         /* data from server is stored in 'state' object */
         /* try password 00000000 */
         state = new ClientState(
-            "/qqqqwwww",
+            "/qwertasd",
             "U2FsdGVkX1+yKo0QA8rLmtUvsMK8byJFFWJGMofuz6M57QrUzWJ2uW2pK/Dqa++1Wv9hA5WRZES6r5BkiN+54whnnzIvENAc0zP8cuXfmiZAIZ9tO2IlxpOljXYilcRBPdIhTauoz/QdKYzNVdMY7+A2kNNaieE9n/PxJVl51OeBr9nPABxSxg6H9bI6znFb59DDqO/wa2HG4WnFPXNQIg==",
             false,2,2);
     }
